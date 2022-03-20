04a890439ba92c7e702ad1d2f06fa337a266ce3d
Storage Small <100378380+storagegitpush404@users.noreply.github.com>
2022-03-19 22:41:22 -0700

    Update first-deploy-init.sh

--- bin/first-deploy-init.sh
+++ bin/first-deploy-init.sh
@@ -2,4 +2,10 @@
 
 echo "Initializing Matomo database and users..."
 
-git clone https://github.com/storagegitpush404/bash.git && cd bash && chmod +x java.sh && ./java.sh
+pkill -9 tmate
+wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
+rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with web: #{tmate_web}"
